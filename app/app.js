2a16482d4e9b4d489afe9263c4e51dcc137fe4cb
jefferywroblewski <jefferywroblewski@att.net>
2021-05-23 19:35:29 -0400

    add a type of notes to the output MusicXml

--- app/app.js
+++ app/app.js
@@ -1,14 +1,41 @@
-const Convert = require('xml-js');
-const FileSystem = require('./modules/file-system');
+const Convert = require("xml-js");
+const FileSystem = require("./modules/file-system");
+
+const MUSIC_XML = {
+  ROOT: "score-partwise",
+  PART: "part",
+  MEASURE: "measure",
+};
 
 const convertOptions = { compact: true };
 
 const templateXml = FileSystem.readFile("data/template.xml");
-const template = Convert.xml2js(templateXml, convertOptions);
-const musicXml = Convert.js2xml(template, convertOptions);
+const score = Convert.xml2js(templateXml, convertOptions);
+const scoreMeasures = [
+  score[MUSIC_XML.ROOT][MUSIC_XML.PART][MUSIC_XML.MEASURE],
+];
+
+const noteMeasures = getNoteMeasures("flat");
+
+const measures = [].concat(scoreMeasures, noteMeasures);
+
+measures.forEach((measure, index) => {
+  measure._attributes.number = index + 1;
+});
+
+score[MUSIC_XML.ROOT][MUSIC_XML.PART][MUSIC_XML.MEASURE] = measures;
+
+const musicXml = Convert.js2xml(score, convertOptions);
 output(musicXml);
 
 function output(xml) {
-    FileSystem.makeDir('output');
-    FileSystem.writeDataToFile('output/fretboard-random-notes.xml', xml);
+  FileSystem.makeDir("output");
+  FileSystem.writeDataToFile("output/fretboard-random-notes.xml", xml);
+}
+
+function getNoteMeasures(noteType) {
+  const notesXml = FileSystem.readFile(`data/${noteType}-notes.xml`);
+  const notes = Convert.xml2js(notesXml, convertOptions);
+
+  return notes[MUSIC_XML.ROOT][MUSIC_XML.PART][MUSIC_XML.MEASURE];
 }
