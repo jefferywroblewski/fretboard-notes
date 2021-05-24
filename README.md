d262c807bb1430c199c31fb35c769f38e76c1cc4
jefferywroblewski <jefferywroblewski@att.net>
2021-05-23 20:50:08 -0400

    Goals of project completed

--- README.md
+++ README.md
@@ -1,42 +1,42 @@
 # fretboard-notes
+This project was created to help generate exercises that can be played in music software to aid in the memorization of every note on the guitar fretboard.   
+
+The code is not yet super robust, it just gets the job done within the limited scope defined in the Project Goal.
 
 # Project Goal
-* This NodeJS app generates a MusicXml file that can be imported into Guitar Pro
+* This NodeJS app generates a MusicXml file that can be imported into a music application.  
 
 * The MusicXml file will have a consistent structure:
-    *  6/4 time
+    *  The instrumentation is intended for a six-string guitar in standard A440 Hz tuning.
+    *  6/4 time signature
     *  always starts in the key of C
     *  always has a leading "header" measure of quarter note rests
 
-* the file will contain a random selection of seven different notes
-* The notes ascending pattern will alternate from up to down
+* the file will contain a random order of different note combinations
+* The notes ascending pattern will alternate every measure between ascending and descending
 * notes can optionally be all natural, all enharmonic or a mix of natural and enharmonic notes
+* everytime the application runs a new `output/fretboard-random-notes.xml` file is generated or replaced.
 
-# Programming Conventions:
-* xml is read or written to files on disk
-* xml files will be read from file to get the string representation of the xml
-* no XmlDOM will be used in this project
-* all xml string values from files will be converted to JS for use
-* JS objects are converted to xml strings which will then get written to a file as its output
+# How it works
+The measures that include the note patterns for the exercises are already defined in MusicXml in the `data/notes.xml` file.
+Each measure in this file has an id so they can be selected.
+The application uses the `data/template.xml` file for a basis.
+It then uses the specified notes to include to find the measures to include from the notse.xml.
+It adds these measures to the template's list of measures to get the final MusicXml to be saved to disk.
 
-# Phases
+# Setup
+Run `npm install` to install project dependencies
 
-## Phase I - Template MusicXml
-Just get the template MusicXml to import and export as-is and make sure it works with Guitar Pro. 
-* create a template MusicXml file that has all the required static content 
-* read the template MusicXml from disk
-* convert it to JS  
-* convert it back to a xml string
-* output xml string as a new MusicXml file 
+# Execution
+To run the program to produce the output music xml:
+* in the `app/app.js` file, edit the `notes` array to specify the type of notes to be included.
+    * the type of notes are listed in the `NOTES` constants.
+* save the `app/app.js` file
+* generate the output xml file using the following command: `npm run start`   
+* After the xml file is generated, import the xml into the application of your choice to view the exercise score.
 
-Acceptance Criteria:
-* The new MusicXml file can be imported into Guitar Pro
-* Guitar Pro displays the content of the MusicXml:
-    * Title
-    * Sub-title
-    * Tempo will be the GP default because this information is not stored with MusicXml
-    * The first measure contains:
-        * Time Signature of 6/4
-        * Key is C
-        * 6 quarter note rests
-    
+# MusicXml
+The output format of the file is MusicXml. See https://www.musicxml.com/   
+
+This format was chosen because it is accepted in a wide array of music applications and has a strictly defined schema. 
+
--- app/app.js
+++ app/app.js
@@ -1,41 +1,110 @@
-const Convert = require("xml-js");
-const FileSystem = require("./modules/file-system");
-
-const MUSIC_XML = {
-  ROOT: "score-partwise",
-  PART: "part",
-  MEASURE: "measure",
+const NOTES = {
+  FLAT: ["Bflat", "Eflat", "Aflat", "Dflat", "Gflat"],
+  NATURAL: ["A", "B", "C", "D", "E", "F", "G"],
+  SHARP: ["Fsharp", "Csharp", "Gsharp", "Dsharp", "Asharp"],
 };
 
-const convertOptions = { compact: true };
+/**
+ * Add/remove the different NOTES constants to include in the output
+ * in the concat statement below.
+ * Note: flat and sharp are used instead of the typical (b) and (#) notations
+ * because the MusicXml schema does not allow the "#" character in its measure id attribute.
+ * Since these values are used to build the measure id that we use to find the measures to include,
+ * we have to spell them out.
+ */
+const notes = [].concat(NOTES.NATURAL);
+console.log('Notes to be included:', notes);
 
-const templateXml = FileSystem.readFile("data/template.xml");
-const score = Convert.xml2js(templateXml, convertOptions);
-const scoreMeasures = [
-  score[MUSIC_XML.ROOT][MUSIC_XML.PART][MUSIC_XML.MEASURE],
-];
+createRandomNotesMusicXml(notes);
 
-const noteMeasures = getNoteMeasures("flat");
+function createRandomNotesMusicXml(notes) {
+  const Convert = require("xml-js");
+  const FileSystem = require("./modules/file-system");
 
-const measures = [].concat(scoreMeasures, noteMeasures);
+  const MUSIC_XML = {
+    ROOT: "score-partwise",
+    PART: "part",
+    MEASURE: "measure",
+  };
+  const convertOptions = { compact: true };
 
-measures.forEach((measure, index) => {
-  measure._attributes.number = index + 1;
-});
+  const templateXml = FileSystem.readFile("data/template.xml");
+  const score = Convert.xml2js(templateXml, convertOptions);
+  const scoreMeasures = [
+    score[MUSIC_XML.ROOT][MUSIC_XML.PART][MUSIC_XML.MEASURE],
+  ];
 
-score[MUSIC_XML.ROOT][MUSIC_XML.PART][MUSIC_XML.MEASURE] = measures;
+  const noteMeasures = getNoteMeasures(notes);
 
-const musicXml = Convert.js2xml(score, convertOptions);
-output(musicXml);
+  const measures = [].concat(scoreMeasures, noteMeasures);
 
-function output(xml) {
-  FileSystem.makeDir("output");
-  FileSystem.writeDataToFile("output/fretboard-random-notes.xml", xml);
-}
+  measures.forEach((measure, index) => {
+    measure._attributes.number = index + 1;
+  });
 
-function getNoteMeasures(noteType) {
-  const notesXml = FileSystem.readFile(`data/${noteType}-notes.xml`);
-  const notes = Convert.xml2js(notesXml, convertOptions);
+  score[MUSIC_XML.ROOT][MUSIC_XML.PART][MUSIC_XML.MEASURE] = measures;
 
-  return notes[MUSIC_XML.ROOT][MUSIC_XML.PART][MUSIC_XML.MEASURE];
+  const musicXml = Convert.js2xml(score, convertOptions);
+  output(musicXml);
+
+  function output(xml) {
+    const outputFile = "output/fretboard-random-notes.xml";
+    FileSystem.makeDir("output");
+    FileSystem.writeDataToFile(outputFile, xml);
+    console.log(`${outputFile} created successfully!`);
+  }
+
+  function getNoteMeasures(notesToInclude) {
+    const ids = getMeasureIds(notesToInclude);
+    const notesXml = FileSystem.readFile(`data/notes.xml`);
+    const notes = Convert.xml2js(notesXml, convertOptions);
+    const measures = notes[MUSIC_XML.ROOT][MUSIC_XML.PART][MUSIC_XML.MEASURE];
+
+    return ids.reduce((acc, id) => {
+      const measureToInclude = measures.find(
+        (measure) => measure._attributes.id === id
+      );
+
+      if (!!measureToInclude) {
+        acc.push(measureToInclude);
+      }
+
+      return acc;
+    }, []);
+  }
+
+  function getMeasureIds(notesToInclude) {
+    return shuffle(notesToInclude).map((note, index) => {
+      const odd = (index + 1) % 2;
+      const direction = odd ? "up" : "down";
+
+      return `${note}-${direction}`;
+    });
+  }
+
+  /**
+   * The de-facto unbiased shuffle algorithm is the Fisher-Yates (aka Knuth) Shuffle
+   * https://github.com/coolaj86/knuth-shuffle
+   * @param array
+   * @returns array
+   */
+  function shuffle(array) {
+    let currentIndex = array.length;
+    let temporaryValue;
+    let randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
 }
